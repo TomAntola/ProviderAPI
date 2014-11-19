5cf616d1c4f649acb54dc3550a2374e493e157e9
Tom Antola <tomantola@taximagic.com>
2014-11-19 17:43:49 -0500

    Added ProviderContext and LoadAppConfig.

---
+++ src/app/DAL/ProviderContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DAL
+{
+    public class ProviderContext : DbContext
+    {
+        public ProviderContext()
+        {
+            Database.SetInitializer<ProviderContext>(null);
+            this.Database.Connection.ConnectionString = LoadAppConfig.ConnectionString;
+            var objectContext = (this as IObjectContextAdapter).ObjectContext;
+            objectContext.CommandTimeout = LoadAppConfig.CommandTimeout;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
