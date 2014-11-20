3fa0a23b1b5bfd47f4bd0a176fe40bd885ea972f
Tom Antola <tomantola@taximagic.com>
2014-11-20 18:32:30 -0500

    Switch SQL command from using T-SQL to using stored procedure.

--- src/app/DAL/Repositories/VehicleRepository.cs
+++ src/app/DAL/Repositories/VehicleRepository.cs
@@ -1,12 +1,13 @@
 ﻿using Common.HttpStatusCodeExceptions;
 using DAL.Entities;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace DAL.Repositories
 {
     public class VehicleRepository
     {
-        private const string GET_VEHICLE = "select p.provider_name Provider, null Company, v.provider_vehicle_id CarNo, vt.vehicle_type VehicleType, v.max_capacity MaxNoOfPassengers, v.is_active IsActive from vehicle v inner join provider p on v.provider_id = p.provider_id inner join vehicle_type vt on v.vehicle_type_id = vt.vehicle_type_id where p.provider_name = '{0}' and v.provider_vehicle_id = '{1}'";
+        private const string GET_VEHICLE = "execute dbo.GetVehicle '{0}', '{1}', '{2}';";
 
         public Vehicle GetVehicle(string provider, string company, string carNo)
         {
@@ -14,7 +15,7 @@ namespace DAL.Repositories
 
             using (var context = new ProviderContext())
             {
-                vehicle = context.Database.SqlQuery<Vehicle>(string.Format(GET_VEHICLE, provider, carNo)).FirstOrDefault();
+                vehicle = context.Database.SqlQuery<Vehicle>(string.Format(GET_VEHICLE, provider, company, carNo)).FirstOrDefault();
             }
 
             if (vehicle == null)
