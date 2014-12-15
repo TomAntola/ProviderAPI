2b2851911e6af6e36909dc93681dd35c4e0b4ee7
Tom Antola <tomantola@taximagic.com>
2014-12-15 15:14:07 -0500

    Added authentication failure messages.

--- src/app/Services/Security/SecurityService.cs
+++ src/app/Services/Security/SecurityService.cs
@@ -9,8 +9,8 @@ namespace Services.Security
     public class SecurityService : ISecurityService
     {
         private const int SALT_BYTE_SIZE = 64;
-        private const int MINIMUM_SALT_LENGTH = 1;
-        private const int MINIMUM_PASSWORD_LENGTH = 1;
+        private const int MINIMUM_SALT_LENGTH = 8;
+        private const int MINIMUM_PASSWORD_LENGTH = 8;
 
         private readonly IProviderApiUserRepository _providerApiUserRepository;
 
@@ -43,7 +43,7 @@ namespace Services.Security
 
             if (Password.Length < MINIMUM_PASSWORD_LENGTH)
             {
-                throw new ArgumentException(string.Format("Password is required to be at {0} one characters in length.", MINIMUM_PASSWORD_LENGTH));
+                throw new ArgumentException(string.Format("Password is required to be at least {0} characters in length.", MINIMUM_PASSWORD_LENGTH));
             }
 
             if (Salt == null)
@@ -67,7 +67,7 @@ namespace Services.Security
             return HashAlgorithm.ComputeHash(passwordWithSalt);
         }
 
-        public bool ValidPassword(byte[] AttemptedPassword, byte[] ActualPassword)
+        public bool IsValidPassword(byte[] AttemptedPassword, byte[] ActualPassword)
         {
             #region Parameter Validation.
             if (AttemptedPassword == null)
